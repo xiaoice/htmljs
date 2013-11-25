5a83736d74ece237f36059cb4b3181d4599b9c4f
xinyu198736 <xinyu198736@gmail.com>
2013-11-25 23:33:22 +0800

    订阅专栏

--- run.js
+++ run.js
@@ -2,8 +2,9 @@
 //var API_KEY = '824cf53f197167cd94ba0815649d3749';
 //require('strong-agent').profile(API_KEY, APPLICATION_NAME);
 global.console.log = function(data){
-    var now = new Date()
-    console.info(now.getFullYear()+"-"+(now.getMonth()+1)+"-"+now.getDate()+" "+now.getHours()+":"+now.getMinutes()+":"+now.getSeconds()+"   "+data)
+   var now = new Date()
+   console.info(now.getFullYear()+"-"+(now.getMonth()+1)+"-"+now.getDate()+" "+now.getHours()+":"+now.getMinutes()+":"+now.getSeconds())
+   console.info(data);
 }
 require ("coffee-script");
 require ("iced-coffee-script");
@@ -14,5 +15,5 @@ var server = require ("./index.coffee")
 
 require('http').createServer(server).listen(server.get("port"),function(){
     console.log("Express server listening on port " + server.get("port"));
-  });
+  }).setMaxListeners(0);
 
